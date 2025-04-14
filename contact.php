81e80dfe91285411bed0b45fd3215c783f1aacee
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-04-14 08:55:02 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,28 @@
+<html>
+	<?php require_once ('inc/head.php')?>
+<body>
+	<?php require_once ('inc/menu.php')?>
+	<div class="container">
+		<div class="pages">
+
+		<!--------	Contact	-------->
+			<h1>Contact Us</h1>
+			 <div class="column">
+    <div class="card">
+      <img src="https://i.ibb.co/Nnbsdv46/Polish-20250409-212005759.png" alt="Tcroneb" style="width:100%">
+      <div class="container">
+        <h2>TCB</h2>
+        <p class="title">Network proffetional </p>
+        <p>paid tech zone</p>
+        <p><button class="button"><a href="https://tcroneb.zone.id">Contact</a></button></p>
+      </div>
+    </div>
+  </div>
+			
+		<!--------	END Contact	-------->
+
+		</div>
+	</div>	
+<?php require_once ('inc/footer.php')?>		
+</body>
+</html>
